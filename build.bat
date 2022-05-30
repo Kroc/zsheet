52769dc445f27d91c441d255a18ae493d4dfb94b
Kroc Camen <kroc@camendesign.com>
2022-05-30 11:57:05 +0100

    Add Exomizer to the build
    This compresses the main executable, but later on we will use it to combine the font into the main payload whilst also being able to relocate the main code to $C000.
    
    Font is now loaded from disk, but isn't used yet.

--- build.bat
+++ build.bat
@@ -8,6 +8,8 @@ SET WLA_LINK="bin\wla-dx\wlalink.exe"    -i -A -S
 
 REM # combine the CPU assembler and system symbols for a C64
 SET WLA_C64=%WLA_6510% -D SYSTEM_CBM=1 -D SYSTEM_C64=1
+REM # packer for compressing programs
+SET EXOMIZER="bin\exomizer\exomizer.exe"
 REM # utility to pack C64 binaries onto a C64 disk-image
 SET C1541="bin\vice\c1541.exe"
 REM # C64 emulator
@@ -27,11 +29,19 @@ IF ERRORLEVEL 1 EXIT /B 1
 
 IF ERRORLEVEL 1 EXIT /B 1
 
+REM # compress the program
+ECHO Exomizer...
+%EXOMIZER% sfx sys -t64 -n -q ^
+     -o "build\zsheet_c64-exo.prg" ^
+     -- "build\zsheet_c64.prg
+
+IF ERRORLEVEL 1 EXIT /B 1
+
 REM # build a 1541 floppy disk image
 %C1541% ^
     -format "zsheet,00" d64 "build/zsheet-c64.d64" ^
-    -write  "build/zsheet_c64.prg"  "zsheet" ^
-    -write  "font/zsheet_c64.bin"   "font"
+    -write  "build/zsheet_c64-exo.prg"  "zsheet" ^
+    -write  "font/zsheet_c64.bin"       "font"
 
 IF ERRORLEVEL 1 EXIT /B 1
 
